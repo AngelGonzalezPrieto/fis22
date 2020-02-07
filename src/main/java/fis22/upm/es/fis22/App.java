64df5f671ddeb4125cfc7118194147fdb5ad5aa6
daniel.lopez <daniel.lopez@sala8pc01>
2020-02-07 12:47:44 +0100

    Primera subida

---
+++ src/main/java/fis22/upm/es/fis22/App.java
@@ -0,0 +1,13 @@
+package fis22.upm.es.fis22;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
